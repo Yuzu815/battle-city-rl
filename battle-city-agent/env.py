bb1cfaad44eb087053b1e3fd603ac31740a631b8
vegetable1024 <vegetable1024@outlook.com>
2023-04-08 16:39:48 +0800

    Extract the custom environment as env.py.

---
+++ battle-city-agent/env.py
@@ -0,0 +1,82 @@
+import copy
+import time
+import gymnasium as gym
+import numpy as np
+from utils import create_agent, get_agent_data_by_json, send_command_to_agent, update_canvas
+
+
+class TankEnv(gym.Env):
+    metadata = {"render_modes": ["rgb_array"], "render_fps": 4}
+
+    multiple = 3  # 放大倍数，用于更明显地体现实数坐标的差异
+    length = 260  # 游戏默认的画布大小
+    uuid = None  # 对应远程环境的uuid
+    canvas = None
+
+    penalty = 5  # 游戏步数的惩罚系数
+
+    level = 1  # 游戏默认值
+    HP = 2  # 游戏默认值
+    score = 0  # 游戏默认值
+
+    def __init__(self):
+        super().__init__()
+        # 初始化环境
+        _low = 0
+        _high = self.length * self.multiple
+        self.observation_space = gym.spaces.Box(low=_low, high=_high, shape=(_high, _high, 3))
+        self.action_space = gym.spaces.Discrete(5)  # 上，下，左，右，开火
+        self.obs = 255 * np.ones(shape=[_high, _high, 3], dtype=np.uint8)
+        self.done = False
+
+    def step(self, action):
+        map_action = ['w', 'a', 's', 'd', 'j']
+        send_command_to_agent(key=map_action[action], uuid=self.uuid)
+        reward = self.update()
+        obs = self._get_observation()
+        return copy.deepcopy(obs), reward, self.done, False, {}
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self.uuid = create_agent()
+        # 等待远程环境初始化完成
+        while True:
+            response_dict = get_agent_data_by_json(uuid=self.uuid)
+            if response_dict['result'] \
+                    and ('Player' in response_dict['data']) \
+                    and len(response_dict['data']['Player']) > 0:
+                break
+            time.sleep(1)
+        self.HP = 2
+        self.level = 1
+        self.score = 0
+        self.update()
+        return copy.deepcopy(self._get_observation()), {}
+
+    def update(self):
+        response_dict = get_agent_data_by_json(uuid=self.uuid)
+        update_canvas(response_dict, self.canvas, self.multiple)
+        Score = response_dict['data']['Score']
+        Done = response_dict['data']['Done']
+        HP = response_dict['data']['HP']
+        reward = -self.penalty
+        if Done:
+            reward = reward - 500
+        if self.score < Score:
+            reward = reward + (Score - self.score)
+        if HP < self.HP:
+            reward = reward - 200 * (HP - self.HP)
+            self.HP = HP
+        self.done = response_dict['data']['Done']
+        return reward
+
+    def _get_observation(self):
+        response_dict = get_agent_data_by_json(uuid=self.uuid)
+        update_canvas(response_dict, self.canvas, self.multiple)
+        return self.canvas
+
+    def close(self):
+        super().close()
+
+    def render(self):
+        pass
--- battle-city-agent/train.py
+++ battle-city-agent/train.py
@@ -1,95 +1,262 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
 import os
-import time
+import pprint
+
 import gymnasium as gym
 import numpy as np
-
 import torch
-from tianshou.utils import TensorboardLogger
 from torch.utils.tensorboard import SummaryWriter
-from tianshou.data import Collector, ReplayBuffer, VectorReplayBuffer
-from tianshou.env import SubprocVectorEnv, DummyVectorEnv
-from tianshou.policy import DQNPolicy
-from tianshou.trainer import offpolicy_trainer
-from tianshou.utils.net.common import Net
 
-from utils import create_agent, get_agent_data_by_json, send_command_to_agent, update_canvas
+from tianshou.data import Collector
+from tianshou.env import SubprocVectorEnv
+from tianshou.policy import BCQPolicy
+from tianshou.trainer import offline_trainer
+from tianshou.utils import TensorboardLogger, WandbLogger
+from tianshou.utils.net.common import MLP, Net
+from tianshou.utils.net.continuous import VAE, Critic, Perturbation
 
+from typing import Tuple
+import d4rl
+import h5py
+from tianshou.data import ReplayBuffer
+from tianshou.utils import RunningMeanStd
 
-class Tank(gym.Env):
-    metadata = {"render_modes": ["rgb_array"], "render_fps": 4}
-
-    multiple = 3  # 放大倍数，用于更明显地体现实数坐标的差异
-    length = 260  # 游戏默认的画布大小
-    uuid = None  # 对应远程环境的uuid
-    canvas = None
-
-    penalty = 5  # 游戏步数的惩罚系数
-
-    level = 1  # 游戏默认值
-    HP = 2  # 游戏默认值
-    score = 0  # 游戏默认值
-
-    def __init__(self):
-        super().__init__()
-        # 初始化环境
-        _low = 0
-        _high = self.length * self.multiple
-        self.observation_space = gym.spaces.Box(low=_low, high=_high, shape=(_high, _high, 3))
-        self.action_space = gym.spaces.Discrete(5)  # 上，下，左，右，开火
-        self.obs = 255 * np.ones(shape=[_high, _high, 3], dtype=np.uint8)
-        self.done = False
-
-    def step(self, action):
-        map_action = ['w', 'a', 's', 'd', 'j']
-        send_command_to_agent(key=map_action[action], uuid=self.uuid)
-        reward = self.update()
-        obs = self._get_observation()
-        return obs, reward, self.done, False, {}
-
-    def reset(self, seed=None, options=None):
-        self.uuid = create_agent()
-        # 等待远程环境初始化完成
-        while True:
-            response_dict = get_agent_data_by_json(uuid=self.uuid)
-            if response_dict['result'] \
-                    and ('Player' in response_dict['data']) \
-                    and len(response_dict['data']['Player']) > 0:
-                break
-            time.sleep(1)
-        self.HP = 2
-        self.level = 1
-        self.score = 0
-        self.update()
-        return self._get_observation(), {}
-
-    def update(self):
-        response_dict = get_agent_data_by_json(uuid=self.uuid)
-        update_canvas(response_dict, self.canvas, self.multiple)
-        Score = response_dict['data']['Score']
-        Done = response_dict['data']['Done']
-        HP = response_dict['data']['HP']
-        reward = -self.penalty
-        if Done:
-            reward = reward - 500
-        if self.score < Score:
-            reward = reward + (Score - self.score)
-        if HP < self.HP:
-            reward = reward - 200 * (HP - self.HP)
-            self.HP = HP
-        self.done = response_dict['data']['Done']
-        return reward
-
-    def _get_observation(self):
-        response_dict = get_agent_data_by_json(uuid=self.uuid)
-        update_canvas(response_dict, self.canvas, self.multiple)
-        return self.canvas
-
-    def close(self):
-        super().close()
-
-    def render(self):
-        pass
+from env import TankEnv
 
 
-if __name__ == '__main__':
-    pass
+def load_buffer_d4rl(expert_data_task: str) -> ReplayBuffer:
+    dataset = d4rl.qlearning_dataset(gym.make(expert_data_task))
+    replay_buffer = ReplayBuffer.from_data(
+        obs=dataset["observations"],
+        act=dataset["actions"],
+        rew=dataset["rewards"],
+        done=dataset["terminals"],
+        obs_next=dataset["next_observations"],
+        terminated=dataset["terminals"],
+        truncated=np.zeros(len(dataset["terminals"]))
+    )
+    return replay_buffer
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=str, default="HalfCheetah-v2")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument(
+        "--expert-data-task", type=str, default="halfcheetah-expert-v2"
+    )
+    parser.add_argument("--buffer-size", type=int, default=1000000)
+    parser.add_argument("--hidden-sizes", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--actor-lr", type=float, default=1e-3)
+    parser.add_argument("--critic-lr", type=float, default=1e-3)
+    parser.add_argument("--start-timesteps", type=int, default=10000)
+    parser.add_argument("--epoch", type=int, default=200)
+    parser.add_argument("--step-per-epoch", type=int, default=5000)
+    parser.add_argument("--n-step", type=int, default=3)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--test-num", type=int, default=10)
+    parser.add_argument("--logdir", type=str, default="log")
+    parser.add_argument("--render", type=float, default=1 / 35)
+
+    parser.add_argument("--vae-hidden-sizes", type=int, nargs="*", default=[512, 512])
+    # default to 2 * action_dim
+    parser.add_argument("--latent-dim", type=int)
+    parser.add_argument("--gamma", default=0.99)
+    parser.add_argument("--tau", default=0.005)
+    # Weighting for Clipped Double Q-learning in BCQ
+    parser.add_argument("--lmbda", default=0.75)
+    # Max perturbation hyper-parameter for BCQ
+    parser.add_argument("--phi", default=0.05)
+    parser.add_argument(
+        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
+    )
+    parser.add_argument("--resume-path", type=str, default=None)
+    parser.add_argument("--resume-id", type=str, default=None)
+    parser.add_argument(
+        "--logger",
+        type=str,
+        default="tensorboard",
+        choices=["tensorboard", "wandb"],
+    )
+    parser.add_argument("--wandb-project", type=str, default="offline_d4rl.benchmark")
+    parser.add_argument(
+        "--watch",
+        default=False,
+        action="store_true",
+        help="watch the play of pre-trained policy only",
+    )
+    return parser.parse_args()
+
+
+def test_bcq():
+    args = get_args()
+    # env = gym.make(args.task)
+    env = TankEnv()
+    args.state_shape = env.observation_space.shape or env.observation_space.n
+    args.action_shape = env.action_space.shape or env.action_space.n
+    args.max_action = env.action_space.high[0]  # float
+    print("device:", args.device)
+    print("Observations shape:", args.state_shape)
+    print("Actions shape:", args.action_shape)
+    print("Action range:", np.min(env.action_space.low), np.max(env.action_space.high))
+
+    args.state_dim = args.state_shape[0]
+    args.action_dim = args.action_shape[0]
+    print("Max_action", args.max_action)
+
+    # test_envs = gym.make(args.task)
+    test_envs = SubprocVectorEnv(
+        [lambda: gym.make(args.task) for _ in range(args.test_num)]
+    )
+    # seed
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    test_envs.seed(args.seed)
+
+    # model
+    # perturbation network
+    net_a = MLP(
+        input_dim=args.state_dim + args.action_dim,
+        output_dim=args.action_dim,
+        hidden_sizes=args.hidden_sizes,
+        device=args.device,
+    )
+    actor = Perturbation(
+        net_a, max_action=args.max_action, device=args.device, phi=args.phi
+    ).to(args.device)
+    actor_optim = torch.optim.Adam(actor.parameters(), lr=args.actor_lr)
+
+    net_c1 = Net(
+        args.state_shape,
+        args.action_shape,
+        hidden_sizes=args.hidden_sizes,
+        concat=True,
+        device=args.device,
+    )
+    net_c2 = Net(
+        args.state_shape,
+        args.action_shape,
+        hidden_sizes=args.hidden_sizes,
+        concat=True,
+        device=args.device,
+    )
+    critic1 = Critic(net_c1, device=args.device).to(args.device)
+    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=args.critic_lr)
+    critic2 = Critic(net_c2, device=args.device).to(args.device)
+    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=args.critic_lr)
+
+    # vae
+    # output_dim = 0, so the last Module in the encoder is ReLU
+    vae_encoder = MLP(
+        input_dim=args.state_dim + args.action_dim,
+        hidden_sizes=args.vae_hidden_sizes,
+        device=args.device,
+    )
+    if not args.latent_dim:
+        args.latent_dim = args.action_dim * 2
+    vae_decoder = MLP(
+        input_dim=args.state_dim + args.latent_dim,
+        output_dim=args.action_dim,
+        hidden_sizes=args.vae_hidden_sizes,
+        device=args.device,
+    )
+    vae = VAE(
+        vae_encoder,
+        vae_decoder,
+        hidden_dim=args.vae_hidden_sizes[-1],
+        latent_dim=args.latent_dim,
+        max_action=args.max_action,
+        device=args.device,
+    ).to(args.device)
+    vae_optim = torch.optim.Adam(vae.parameters())
+
+    policy = BCQPolicy(
+        actor,
+        actor_optim,
+        critic1,
+        critic1_optim,
+        critic2,
+        critic2_optim,
+        vae,
+        vae_optim,
+        device=args.device,
+        gamma=args.gamma,
+        tau=args.tau,
+        lmbda=args.lmbda,
+    )
+
+    # load a previous policy
+    if args.resume_path:
+        policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
+        print("Loaded agent from: ", args.resume_path)
+
+    # collector
+    test_collector = Collector(policy, test_envs)
+
+    # log
+    now = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+    args.algo_name = "bcq"
+    log_name = os.path.join(args.task, args.algo_name, str(args.seed), now)
+    log_path = os.path.join(args.logdir, log_name)
+
+    # logger
+    if args.logger == "wandb":
+        logger = WandbLogger(
+            save_interval=1,
+            name=log_name.replace(os.path.sep, "__"),
+            run_id=args.resume_id,
+            config=args,
+            project=args.wandb_project,
+        )
+    writer = SummaryWriter(log_path)
+    writer.add_text("args", str(args))
+    if args.logger == "tensorboard":
+        logger = TensorboardLogger(writer)
+    else:  # wandb
+        logger.load(writer)
+
+    def save_best_fn(policy):
+        torch.save(policy.state_dict(), os.path.join(log_path, "policy.pth"))
+
+    def watch():
+        if args.resume_path is None:
+            args.resume_path = os.path.join(log_path, "policy.pth")
+
+        policy.load_state_dict(
+            torch.load(args.resume_path, map_location=torch.device("cpu"))
+        )
+        policy.eval()
+        collector = Collector(policy, env)
+        collector.collect(n_episode=1, render=1 / 35)
+
+    if not args.watch:
+        replay_buffer = load_buffer_d4rl(args.expert_data_task)
+        # trainer
+        result = offline_trainer(
+            policy,
+            replay_buffer,
+            test_collector,
+            args.epoch,
+            args.step_per_epoch,
+            args.test_num,
+            args.batch_size,
+            save_best_fn=save_best_fn,
+            logger=logger,
+        )
+        pprint.pprint(result)
+    else:
+        watch()
+
+    # Let's watch its performance!
+    policy.eval()
+    test_envs.seed(args.seed)
+    test_collector.reset()
+    result = test_collector.collect(n_episode=args.test_num, render=args.render)
+    print(f"Final reward: {result['rews'].mean()}, length: {result['lens'].mean()}")
+
+
+if __name__ == "__main__":
+    test_bcq()
